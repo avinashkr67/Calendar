8678e4647e7da23a72ed91b5db29f0846943d3b9
Avinash Kumar <official.avinashkr@gmail.com>
2022-02-09 00:48:31 +0530

    Final

---
+++ js/index.js
@@ -0,0 +1,184 @@
+$(document).ready(function(){
+    $(".menu-bar").on("click", function(){
+        $(".nav-menu").toggle();
+    })
+})
+
+$(document).ready(function(){
+    $(".fes1").click(function(){
+       alert("01-01-2022 : New Year");
+    });
+
+    $(".fes2").click(function(){
+       alert("09-01-2022 : Guru Govind Singh Jayanti");
+    });
+
+    $(".fes3").click(function(){
+       alert("13-01-2022 : Lohri");
+    });
+
+    $(".fes4").click(function(){
+       alert("14-01-2022 : Makar Sakranti, Pongal");
+    });
+    
+    $(".fes5").click(function(){
+       alert("26-01-2022 : Republic Day");
+    });
+
+    $(".fes6").click(function(){
+       alert("05-02-2022 : Vasant Panchami");
+    });
+
+    $(".fes7").click(function(){
+       alert("15-02-2022 : Hazarat Ali's Birthday");
+    });
+    
+    $(".fes8").click(function(){
+       alert("16-02-2022 : Guru Ravidas Jayanti");
+    });
+
+    $(".fes9").click(function(){
+       alert("19-02-2022 : Shivaji Jayanti");
+    });
+
+    $(".fes10").click(function(){
+       alert("26-02-2022 :  Maharishi Dayanand Saraswati Jayanti");
+    });
+
+    $(".fes11").click(function(){
+       alert("01-03-2022 : Maha Shivaratri");
+    });
+
+    $(".fes12").click(function(){
+       alert("17-03-2022 : Holika Dahana");
+    });
+
+    $(".fes13").click(function(){
+       alert("18-03-2022 : Dolyatra, Holi");
+    });
+
+    $(".fes14").click(function(){
+       alert("02-04-2022 : Gudi Padwa, Chaitra Sukhladi, ugadi");
+    });
+
+    $(".fes15").click(function(){
+       alert("10-04-2022 : Rama Navami");
+    });
+
+    $(".fes16").click(function(){
+       alert("14-04-2022 : Mahavir jayanti, Vaisakhi");
+    });
+
+    $(".fes17").click(function(){
+       alert("15-04-2022 : Good Friday");
+    });
+
+    $(".fes18").click(function(){
+       alert("17-04-2022 : Easter Day");
+    });
+
+    $(".fes19").click(function(){
+       alert("29-04-2022 : Jamat Ul-Vida");
+    });
+
+    $(".fes20").click(function(){
+       alert("03-05-2022 : Ramzan Id/Eid-ul-Fitar");
+    });
+
+    $(".fes21").click(function(){
+       alert("09-05-2022 : Birthday of Ravindranath");
+    });
+
+    $(".fes22").click(function(){
+       alert("16-05-2022 : Buddha Purnima");
+    });
+
+    $(".fes23").click(function(){
+       alert("01-07-2022 : Rath Yatra");
+    });
+
+    $(".fes24").click(function(){
+       alert("10-07-2022 : Bakr Id/Eid ul-Adha");
+    });
+
+    $(".fes25").click(function(){
+       alert("08-08-2022 : Muharram");
+    });
+
+    $(".fes26").click(function(){
+       alert("11-08-2022 : Raksha Bandhan");
+    });
+
+    $(".fes27").click(function(){
+       alert("15-08-2022 : Independence Day");
+    });
+
+    $(".fes28").click(function(){
+       alert("16-08-2022 : Parsi New Year");
+    });
+
+    $(".fes29").click(function(){
+       alert("19-08-2022 : Janmashtami");
+    });
+
+    $(".fes30").click(function(){
+       alert("31-08-2022 : Ganesh Chaturthi");
+    });
+
+    $(".fes31").click(function(){
+       alert("08-09-2022 : Onam");
+    });
+
+    $(".fes32").click(function(){
+       alert("16-09-2022 : First Day of Sharad navratri");
+    });
+
+    $(".fes33").click(function(){
+       alert("02-10-2022 : Gandhi jayanti");
+    });
+
+    $(".fes34").click(function(){
+       alert("05-10-2022 : Dussera");
+    });
+    
+    $(".fes35").click(function(){
+       alert("09-10-2022 : Maharishi Valmiki Jayanti");
+    });
+
+    $(".fes36").click(function(){
+       alert("13-10-2022 : Karva Chauth");
+    });
+
+    $(".fes37").click(function(){
+       alert("24-10-2022 : Diwali");
+    });
+
+    $(".fes38").click(function(){
+       alert("26-10-2022 : Bhai Duj");
+    });
+
+    $(".fes39").click(function(){
+       alert("30-10-2022 : Chhath Puja");
+    });
+    
+    $(".fes40").click(function(){
+       alert("08-11-2022 : Guru Nanak Jayanti");
+    });
+
+    $(".fes41").click(function(){
+       alert("24-11-2022 : Guru Tegh Bahadur's Martyrdom Day");
+    });
+
+    $(".fes42").click(function(){
+       alert("25-12-2022 : Christmas");
+    });
+
+    $(".fes43").click(function(){
+       alert("29-11-2022 : Guru Govind Singh Jayanti");
+    });
+
+
+
+})
+
+
